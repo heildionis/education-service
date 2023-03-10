58e793919aab3cc9d18c85b64101c132d037d817
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-03-10 21:06:46 +0300

    Config: update config

--- server/config/build/buildResolvers.ts
+++ server/config/build/buildResolvers.ts
@@ -3,6 +3,7 @@ import { type BuildOptions } from './types/config';
 
 export const buildResolvers = (options: BuildOptions): webpack.ResolveOptions => ({
 	extensions: ['.ts', '.js'],
+	preferAbsolute: true,
 	modules: [options.paths.src, 'node_modules'],
 	mainFiles: ['index'],
 	alias: {},
