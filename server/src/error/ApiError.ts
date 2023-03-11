ede8fe4aefb4c1e23fb7923eab19c6d548370c7b
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-03-11 08:41:02 +0300

    Error: create error handling

--- server/src/error/ApiError.ts
+++ server/src/error/ApiError.ts
@@ -1,6 +1,6 @@
 export default class ApiError extends Error {
 	status: number;
-	errors: any;
+	errors: Array<any>;
 
 	constructor (status: number, message: string, errors = []) {
 		super(message);
@@ -9,10 +9,34 @@ export default class ApiError extends Error {
 	}
 
 	static badRequest(message: string, errors = []) {
-		return new ApiError(400, `ERROR: bad request ${message}`, errors);
+		return new ApiError(400, message, errors);
 	}
 
-	static unauthorized() {
-		return new ApiError(401, 'User unauthorized');
+	static unauthorized(message: string, errors= []) {
+		return new ApiError(401, message, errors);
+	}
+
+	static forbidden(message: string, errors = []) {
+		return new ApiError(403, message, errors);
+	}
+
+	static notFound(message: string, errors = []) {
+		return new ApiError(404, message, errors);
+	}
+
+	static notAcceptable(message: string, errors = []) {
+		return new ApiError(406, message, errors);
+	}
+
+	static requestTimeout(message: string, errors = []) {
+		return new ApiError(408, message, errors);
+	}
+
+	static conflict(message: string, errors = []) {
+		return new ApiError(409, message, errors);
+	}
+
+	static locked(message: string, errors = []) {
+		return new ApiError(423, message, errors);
 	}
 }
