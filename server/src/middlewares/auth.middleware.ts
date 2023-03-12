92e4ae9c4fc232c36c356b5bbcbefb0c02071748
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-03-12 10:55:16 +0300

    Middleware: create auth middleware

---
+++ server/src/middlewares/auth.middleware.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from 'express';
+
+import ApiError from '../error/ApiError.js';
+import { tokenService } from '../services/token.service.js';
+
+export const authMiddleware = (req: Request, res: Response, next: Function) => {
+	try {
+		const authorizationHeader = req.headers.authorization;
+		if (!authorizationHeader) {
+			return next(ApiError.unauthorized());
+		}
+
+		const accessToken = authorizationHeader.split(' ')[1];
+		if (!accessToken) {
+			return next(ApiError.unauthorized());
+		}
+
+		const userData = tokenService.validateAccessToken(accessToken);
+		if (!userData) {
+			return next(ApiError.unauthorized());
+		}
+
+		// @ts-ignore
+		req.user = userData;
+		next();
+	} catch (e) {
+		return next(ApiError.unauthorized());
+	}
+};
