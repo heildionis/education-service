f59b884c9c7177cb58b90ea7a11ed5f898b20da4
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-03-12 21:15:27 +0300

    Entity User: create checkAuth, logout thunk, selectors

--- client/src/entities/User/index.ts
+++ client/src/entities/User/index.ts
@@ -1,6 +1,10 @@
+export { logout } from './model/services/logout';
+
+export { checkAuth } from './model/services/checkAuth';
+export { getUserIsLoading } from './model/selectors/getUserIsLoading/getUserIsLoading';
 export { getUserAuthData } from './model/selectors/getUserAuthData/getUserAuthData';
 
-export { UserSchema, User } from './model/types/user';
+export { UserSchema, User, UserResponse } from './model/types/user';
 
 export {
     userActions,
