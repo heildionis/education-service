19028dbc11e013e59a0ab348a46a3ebe294df853
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-03-12 12:48:49 +0300

    App: main layout

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,16 +1,23 @@
-import { LoginForm } from 'features/AuthByUsername';
-import { useEffect } from 'react';
+import { userActions } from 'entities/User';
+import { Suspense, useEffect } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { AppRouter } from './providers/RouteProvider/ui/AppRouter';
 
 export const App = () => {
+    const dispatch = useAppDispatch();
+
     useEffect(() => {
-        console.log('init');
-    }, []);
+        dispatch(userActions.initAuthData());
+    }, [dispatch]);
 
     return (
-        <div className='app'>
-            <div className='wrapper'>
-                <LoginForm />
-            </div>
+        <div className={classNames('app')}>
+            <Suspense fallback=''>
+                <div className='content-page'>
+                    <AppRouter />
+                </div>
+            </Suspense>
         </div>
     );
 };
