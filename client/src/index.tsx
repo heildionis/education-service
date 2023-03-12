6f6de64aac47a58ec3e83a8594fca7d723ed85df
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-03-12 21:16:54 +0300

    Global: add theme provider, error boundary, browser router

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,23 @@
 import { App } from 'app/App';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { StoreProvider } from 'app/providers/StoreProvider';
-import { render } from 'react-dom';
+import { ErrorBoundary } from 'app/providers/ErrorBoundary';
+import { ThemeProvider } from 'app/providers/ThemeProvider';
 import 'app/styles/index.scss';
+import 'shared/config/i18n/i18n';
 
-render(
+const container = document.getElementById('root') || document.body;
+const root = createRoot(container);
+
+root.render(
     <StoreProvider>
-        <App />
+        <BrowserRouter>
+            <ThemeProvider>
+                <ErrorBoundary>
+                    <App />
+                </ErrorBoundary>
+            </ThemeProvider>
+        </BrowserRouter>
     </StoreProvider>,
-    document.getElementById('root'),
 );
