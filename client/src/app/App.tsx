b6863eaeb5e5f3a21584a0d35d1285d2bd72d2d7
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-03-13 21:55:01 +0300

    App: refactor

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,7 +1,9 @@
 import { checkAuth } from 'entities/User';
 import { Suspense, useEffect } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { Container } from 'shared/lib/components/Container/Container';
 import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { Navbar } from 'widgets/Navbar';
 import { AppRouter } from './providers/RouteProvider/ui/AppRouter';
 
 export const App = () => {
@@ -14,9 +16,12 @@ export const App = () => {
     return (
         <div className={classNames('app')}>
             <Suspense fallback=''>
-                <div className='content-page'>
-                    <AppRouter />
-                </div>
+                <Navbar />
+                <Container>
+                    <div className='content-page'>
+                        <AppRouter />
+                    </div>
+                </Container>
             </Suspense>
         </div>
     );
