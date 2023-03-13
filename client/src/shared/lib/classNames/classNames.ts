c6c41af45f905a1a32fc9af9f8c59722e4a06b4c
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-03-13 21:49:06 +0300

    Lib: classnames refactor, add random number function

--- client/src/shared/lib/classNames/classNames.ts
+++ client/src/shared/lib/classNames/classNames.ts
@@ -1,13 +1,15 @@
 export type Mods = Record<string, string | boolean | undefined>
 
-export const classNames = (
+export function classNames(
     cls: string,
     mods: Mods = {},
     additional: Array<string | undefined> = [],
-): string => [
-    cls,
-    ...additional.filter(Boolean),
-    ...Object.entries(mods)
-        .filter(([_, value]) => Boolean(value))
-        .map(([className]) => className),
-].join(' ');
+): string {
+    return [
+        cls,
+        ...additional.filter(Boolean),
+        ...Object.entries(mods)
+            .filter(([_, value]) => Boolean(value))
+            .map(([className]) => className),
+    ].join(' ');
+}
---
+++ client/src/shared/lib/generateRandomNumber/getRandomNumber.test.ts
@@ -0,0 +1,10 @@
+// import { generateRandomNumber } from './generateRandomNumber';
+
+// describe('getRandomNumber.test', () => {
+//     test('to be called and no undefined', () => {
+//         expect(generateRandomNumber()).not.toBeUndefined();
+//         expect(generateRandomNumber()).not.toBeNaN();
+//         expect(generateRandomNumber()).not.toBeNull();
+//     });
+// });
+// //
