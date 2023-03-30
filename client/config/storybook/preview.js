5e61d17552439b23cec3069dc1cdc95e3ef387f6
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-03-30 23:01:27 +0300

    Config: configure webpack storybook, add decorators

--- client/config/storybook/preview.js
+++ client/config/storybook/preview.js
@@ -1,9 +1,15 @@
+import { addDecorator } from '@storybook/react';
+import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator';
+import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator';
+
 export const parameters = {
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
         matchers: {
-            color: /(background|color)$/i,
             date: /Date$/,
         },
     },
 };
+
+addDecorator(StyleDecorator);
+addDecorator(ThemeDecorator('app_light_theme'));
