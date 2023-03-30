9b523da08ab3af95120f948e56956f0d87187e6f
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-03-30 23:27:30 +0300

    Config: create storybook decorators

---
+++ client/src/shared/config/storybook/ThemeDecorator.tsx
@@ -0,0 +1,11 @@
+import { Story } from '@storybook/react';
+import { ThemeProvider } from 'app/providers/ThemeProvider';
+import { Theme } from 'shared/lib/themes';
+
+export const ThemeDecorator = (theme: Theme) => (StoryComponent: Story) => (
+    <ThemeProvider initialTheme={theme}>
+        <div className={`app ${theme}`}>
+            <StoryComponent />
+        </div>
+    </ThemeProvider>
+);
