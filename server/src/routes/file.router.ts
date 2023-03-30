a90528f2aeab772dcc1796a27eb13e699525b41c
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-03-30 10:51:25 +0300

    Middleware: create routes for file: upload, download, delete, get files, create dir

--- server/src/routes/file.router.ts
+++ server/src/routes/file.router.ts
@@ -1,7 +1,11 @@
 import { fileController } from './../controllers/file.controller';
 import { Router } from 'express';
-// import { authMiddleware } from 'middlewares/auth.middleware';
+import { authMiddleware } from '../middlewares/auth.middleware';
 
 export const fileRouter = Router();
 
-// fileRouter.get('/', authMiddleware, fileController.createDir);
+fileRouter.post('/', authMiddleware, fileController.createDir);
+fileRouter.get('/', authMiddleware, fileController.getFiles);
+fileRouter.post('/upload', authMiddleware, fileController.uploadFile);
+fileRouter.post('/download', authMiddleware, fileController.downloadFile);
+fileRouter.delete('/', authMiddleware, fileController.deleteFile);
