a969dd437fbd9f7b27eb3444d049d5df59848605
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-03-31 21:24:43 +0300

    Assets: add icons

--- client/src/shared/assets/icons/index.ts
+++ client/src/shared/assets/icons/index.ts
@@ -1,5 +1,9 @@
 import bmstuLogo from './bmstu.svg';
 import telegramLogo from './telegram.svg';
+import folder from './folder.svg';
+import file from './file.svg';
 
 export const BMSTUIcon = bmstuLogo;
+export const FolderIcon = folder;
 export const TelegramIcon = telegramLogo;
+export const FileIcon = file;
