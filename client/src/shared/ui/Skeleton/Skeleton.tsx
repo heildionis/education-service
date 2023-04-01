580fee12c542d3a90d394ea6b791b1878263bcab
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-04-01 16:00:25 +0300

    UI: create custom skeleton component

---
+++ client/src/shared/ui/Skeleton/Skeleton.tsx
@@ -0,0 +1,55 @@
+import {
+    FC,
+    ReactNode,
+    memo,
+    HTMLAttributes,
+    CSSProperties,
+} from 'react';
+import { Additional, classNames } from 'shared/lib/classNames/classNames';
+import cls from './Skeleton.module.scss';
+
+type SkeletonType = 'circular' | 'rounded' | 'text' | 'rectangular';
+type SkeletonAnimation = 'pulse' | 'wave' | 'disabled';
+
+interface SkeletonProps extends HTMLAttributes<HTMLDivElement> {
+    className?: string;
+    variant?: SkeletonType;
+    width?: number | string;
+    height?: number | string;
+    children?: ReactNode;
+    animation?: SkeletonAnimation;
+    border?: string;
+}
+
+export const Skeleton: FC<SkeletonProps> = memo((props: SkeletonProps) => {
+    const {
+        className,
+        variant = 'rectangular',
+        width,
+        height,
+        children,
+        animation = 'pulse',
+        border,
+        ...otherProps
+    } = props;
+
+    const additional: Additional = [
+        className,
+        cls[variant],
+        cls[animation],
+    ];
+
+    const styles: CSSProperties = {
+        width,
+        height,
+        borderRadius: border,
+    };
+
+    return (
+        <div
+            style={styles}
+            className={classNames(cls.Skeleton, {}, additional)}
+            {...otherProps}
+        />
+    );
+});
