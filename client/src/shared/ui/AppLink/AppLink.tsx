5fc945a66dd4424d3368b7e57f7c683a8488b537
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-04-01 16:02:20 +0300

    UI: create custom link component

---
+++ client/src/shared/ui/AppLink/AppLink.tsx
@@ -0,0 +1,32 @@
+import { FC, ReactNode, memo } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Link, LinkProps } from 'react-router-dom';
+import cls from './AppLink.module.scss';
+
+type AppLinkVariant = 'primary' | 'secondary';
+
+interface AppLinkProps extends LinkProps {
+    className?: string;
+    children: ReactNode;
+    variant: AppLinkVariant;
+}
+
+export const AppLink: FC<AppLinkProps> = memo((props: AppLinkProps) => {
+    const {
+        className,
+        to,
+        children,
+        variant = 'primary',
+        ...otherProps
+    } = props;
+
+    return (
+        <Link
+            to={to}
+            className={classNames(cls.AppLink, {}, [className, cls[variant]])}
+            {...otherProps}
+        >
+            {children}
+        </Link>
+    );
+});
