b6512b925b42d2fc263366aca5cc126242ecaa52
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-04-01 15:57:54 +0300

    Assets: update icons

--- client/src/shared/assets/icons/index.ts
+++ client/src/shared/assets/icons/index.ts
@@ -2,8 +2,14 @@ import bmstuLogo from './bmstu.svg';
 import telegramLogo from './telegram.svg';
 import folder from './folder.svg';
 import file from './file.svg';
+import tile from './tile.svg';
+import list from './menu.svg';
+import sidemenu from './dots.svg';
 
 export const BMSTUIcon = bmstuLogo;
 export const FolderIcon = folder;
 export const TelegramIcon = telegramLogo;
 export const FileIcon = file;
+export const TileIcon = tile;
+export const ListIcon = list;
+export const MenuIcon = sidemenu;
