043b0b9c4054dd6dcf118cbb3267576f96e08ac6
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-04-04 23:34:56 +0300

    Page HomePage: home page refactor

--- client/src/pages/HomePage/ui/HomePage.tsx
+++ client/src/pages/HomePage/ui/HomePage.tsx
@@ -4,12 +4,8 @@ import {
 } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Container } from 'shared/lib/components/Container/Container';
-import { FileViewSelector, getFileView } from 'features/FileViewSelector';
-import { FileEntity } from 'entities/File/model/types/file';
-import { useSelector } from 'react-redux';
 import { Page } from 'widgets/Page';
-import { AppLink } from 'shared/ui/AppLink/AppLink';
-import { RoutePath } from 'shared/config/routes/routes';
+import { Typography } from 'shared/ui/Typography/Typography';
 import cls from './HomePage.module.scss';
 
 interface HomePageProps {
@@ -18,14 +14,11 @@ interface HomePageProps {
 
 const HomePage: FC<HomePageProps> = memo((props) => {
     const { className } = props;
-    const view = useSelector(getFileView);
 
     return (
         <Page className={classNames(cls.HomePage, {}, [className])}>
             <Container>
-                <FileViewSelector />
-                <div className={cls.list} />
-                <AppLink to={RoutePath.file} variant='primary'>File</AppLink>
+                <Typography>Главная страница</Typography>
             </Container>
         </Page>
     );
