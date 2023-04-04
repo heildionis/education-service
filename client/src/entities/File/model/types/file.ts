7e834cea295780c3cb34aec3accbc06e78426ee1
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-04-04 23:11:19 +0300

    File: add types for file entity

---
+++ client/src/entities/File/model/types/file.ts
@@ -0,0 +1,16 @@
+export type FileView = 'list' | 'tile'
+
+export type FileType = 'file' | 'dir'
+
+export interface FileEntity {
+    id: number;
+    name: string;
+    type: FileType;
+    size: number;
+    path: string;
+    createdAt: Date;
+    updatedAt: Date;
+    userId: number;
+    parentId: number;
+    accessLink: null;
+}
