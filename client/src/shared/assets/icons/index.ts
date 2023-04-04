39fbf6bc13ed8697034b53ed19cca771f2817e3a
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-04-04 23:33:50 +0300

    Assets: add icons

--- client/src/shared/assets/icons/index.ts
+++ client/src/shared/assets/icons/index.ts
@@ -5,6 +5,7 @@ import file from './file.svg';
 import tile from './tile.svg';
 import list from './menu.svg';
 import sidemenu from './dots.svg';
+import garbage from './garbage.svg';
 
 export const BMSTUIcon = bmstuLogo;
 export const FolderIcon = folder;
@@ -13,3 +14,4 @@ export const FileIcon = file;
 export const TileIcon = tile;
 export const ListIcon = list;
 export const MenuIcon = sidemenu;
+export const GarbageIcon = garbage;
