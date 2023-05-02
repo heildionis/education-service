53ed8983f8a4797ff526095336f04dad67915bca
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-05-02 20:54:52 +0300

    Config: add aliases for tests

--- client/config/jest/jest.config.ts
+++ client/config/jest/jest.config.ts
@@ -36,6 +36,7 @@ export default {
     moduleNameMapper: {
         '\\.(s?css|less)$': 'identity-obj-proxy',
         '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
+        '^@/(.*)$': '<rootDir>/src/$1',
     },
 
     // Indicates whether the coverage information should be collected while executing the test
