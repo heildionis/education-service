c7d60577411a0f2c279af01ac85f7d58678b918a
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-05-04 20:49:18 +0300

    Feature: refactor auth forms

--- client/src/features/AuthBy/ui/LoginForm/LoginForm.tsx
+++ client/src/features/AuthBy/ui/LoginForm/LoginForm.tsx
@@ -1,4 +1,6 @@
-import { FC, memo, useCallback } from 'react';
+import {
+    FC, memo, useCallback, useMemo,
+} from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 
@@ -7,19 +9,10 @@ import { getLoginUsername } from '../../model/selectors/getLoginUsername/getLogi
 import { loginByUsername } from '../../model/services/loginByUsername';
 import { loginActions, loginReducer } from '../../model/slice/loginSlice';
 
-import cls from './LoginForm.module.scss';
-
+import { AuthCard, AuthInputType } from '@/entities/Auth';
 import { AuhtPersonIcon } from '@/shared/assets/icons';
-import { classNames } from '@/shared/lib/classNames/classNames';
 import { DynamicModuleLoader, ReducerList } from '@/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
 import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
-import { AppLink } from '@/shared/ui/AppLink/AppLink';
-import { Button } from '@/shared/ui/Button/Button';
-import { Card } from '@/shared/ui/Card';
-import { Icon } from '@/shared/ui/Icon/Icon';
-import { Input } from '@/shared/ui/Input/Input';
-import { Column } from '@/shared/ui/Stack';
-import { Typography } from '@/shared/ui/Typography/Typography';
 
 export interface LoginFormProps {
     className?: string;
@@ -45,79 +38,46 @@ const LoginForm: FC<LoginFormProps> = memo((props) => {
         dispatch(loginActions.setPassword(value));
     }, [dispatch]);
 
-    const onLoginClick = async () => {
+    const onLoginClick = useCallback(async () => {
         const result = await dispatch(loginByUsername({ username, password }));
         if (result.meta.requestStatus === 'fulfilled') {
             onSuccess();
         }
-    };
+    }, [dispatch, onSuccess, username, password]);
+
+    const inputs = useMemo((): AuthInputType<string>[] => [
+        {
+            label: t('Почта'),
+            value: username,
+            onChange: onChangeUsername,
+            type: 'text',
+            placeholder: t('Введите имя пользователя'),
+        },
+        {
+            label: t('Пароль'),
+            value: password,
+            onChange: onChangePassword,
+            type: 'password',
+            placeholder: t('Введите пароль'),
+        },
+    ], [
+        onChangeUsername,
+        onChangePassword,
+        username,
+        password,
+        t,
+    ]);
 
     return (
         <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
-            <Card variant='secondary' className={cls.card}>
-                <Column gap='16'>
-                    <Icon Svg={AuhtPersonIcon} variant='soft' />
-                    <Typography
-                        className={cls.title}
-                        color='soft'
-                        align='center'
-                        variant='h5'
-                    >
-                        {t('Войдите и получите доступ к файлам')}
-                    </Typography>
-                </Column>
-            </Card>
-            <Column gap='16' className={classNames(cls.form, {}, [className])} fullWidth>
-                <Column fullWidth align='start' gap='2'>
-                    <Typography
-                        color='disabled'
-                        variant='h5'
-                    >
-                        {t('Почта')}
-                    </Typography>
-                    <Input
-                        fullWidth
-                        placeholder={t<string>('Введите имя пользователя')}
-                        value={username}
-                        onChange={onChangeUsername}
-                    />
-                </Column>
-                <Column fullWidth align='start' gap='2'>
-                    <Typography
-                        color='disabled'
-                        variant='h5'
-                    >
-                        {t('Пароль')}
-                    </Typography>
-                    <Input
-                        fullWidth
-                        type='password'
-                        placeholder={t<string>('Введите пароль')}
-                        value={password}
-                        onChange={onChangePassword}
-                    />
-                </Column>
-                <Button
-                    fullWidth
-                    type='button'
-                    onClick={onLoginClick}
-                    className={cls.btn}
-                >
-                    {t('Войти')}
-                </Button>
-                <Typography
-                    align='center'
-                    className={cls.privacy}
-                    color='disabled'
-                    variant='h6'
-                >
-                    {t('Нажимая кнопку "Войти" вы принимаете')}
-                    <AppLink to='/'>
-                        {' '}
-                        {t('пользовательское соглашение')}
-                    </AppLink>
-                </Typography>
-            </Column>
+            <AuthCard
+                className={className}
+                AuthIcon={AuhtPersonIcon}
+                title={t('Войдите и получите доступ к файлам')}
+                authButtonTitle={t('Войти')}
+                onAuthClick={onLoginClick}
+                inputs={inputs}
+            />
         </DynamicModuleLoader>
     );
 });
--- client/src/features/Registration/ui/RegistrationForm/RegistrationForm.tsx
+++ client/src/features/Registration/ui/RegistrationForm/RegistrationForm.tsx
@@ -2,6 +2,7 @@ import {
     FC,
     memo,
     useCallback,
+    useMemo,
 } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
@@ -12,19 +13,10 @@ import { getRegistrationUsername } from '../../model/selectors/getRegistrationUs
 import { register } from '../../model/services/register';
 import { registrationActions, registrationReducer } from '../../model/slice/registrationSlice';
 
-import cls from './RegistrationForm.module.scss';
-
+import { AuthCard, AuthInputType } from '@/entities/Auth';
 import { AuhtPersonIcon } from '@/shared/assets/icons';
-import { classNames } from '@/shared/lib/classNames/classNames';
 import { DynamicModuleLoader, ReducerList } from '@/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
 import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
-import { AppLink } from '@/shared/ui/AppLink/AppLink';
-import { Button } from '@/shared/ui/Button/Button';
-import { Card } from '@/shared/ui/Card';
-import { Icon } from '@/shared/ui/Icon/Icon';
-import { Input } from '@/shared/ui/Input/Input';
-import { Column } from '@/shared/ui/Stack';
-import { Typography } from '@/shared/ui/Typography/Typography';
 
 interface RegistrationFormProps {
     className?: string;
@@ -60,105 +52,63 @@ const RegistrationForm: FC<RegistrationFormProps> = memo((props) => {
         if (result.meta.requestStatus === 'fulfilled') {
             onSuccess();
         }
-    }, [dispatch, onSuccess, email, password, username]);
+    }, [
+        dispatch,
+        onSuccess,
+        email,
+        password,
+        username,
+    ]);
+
+    const inputs = useMemo((): AuthInputType<string>[] => [
+        {
+            label: t('Имя пользователя'),
+            value: username,
+            onChange: onChangeUsername,
+            type: 'text',
+            placeholder: t('Введите имя пользователя'),
+        },
+        {
+            label: t('Почта'),
+            value: email,
+            onChange: onChangeEmail,
+            type: 'email',
+            placeholder: t('Введите почту'),
+        },
+        {
+            label: t('Пароль'),
+            value: password,
+            onChange: onChangePassword,
+            type: 'password',
+            placeholder: t('Введите пароль'),
+        },
+        {
+            label: t('Повторите пароль'),
+            value: password,
+            onChange: onChangePassword,
+            type: 'password',
+            placeholder: t('Введите повторно пароль'),
+        },
+    ], [
+        onChangeEmail,
+        onChangeUsername,
+        onChangePassword,
+        username,
+        email,
+        password,
+        t,
+    ]);
 
     return (
         <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
-            <Card variant='secondary' className={cls.card}>
-                <Column gap='16'>
-                    <Icon Svg={AuhtPersonIcon} variant='soft' />
-                    <Typography
-                        className={cls.title}
-                        color='soft'
-                        align='center'
-                        variant='h5'
-                    >
-                        {t('Зарегистрируйтесь и получите доступ к файлам')}
-                    </Typography>
-                </Column>
-            </Card>
-            <Column gap='16' fullWidth className={classNames(cls.form, {}, [className])}>
-                <Column fullWidth align='start' gap='2'>
-                    <Typography
-                        color='disabled'
-                        variant='h5'
-                    >
-                        {t('Имя пользователя')}
-                    </Typography>
-                    <Input
-                        placeholder={t<string>('Введите имя пользователя')}
-                        type='text'
-                        value={username}
-                        onChange={onChangeUsername}
-                        fullWidth
-                    />
-                </Column>
-                <Column fullWidth align='start' gap='2'>
-                    <Typography
-                        color='disabled'
-                        variant='h5'
-                    >
-                        {t('Почта')}
-                    </Typography>
-                    <Input
-                        placeholder={t<string>('Введите почту')}
-                        type='text'
-                        value={email}
-                        onChange={onChangeEmail}
-                        fullWidth
-                    />
-                </Column>
-                <Column fullWidth align='start' gap='2'>
-                    <Typography
-                        color='disabled'
-                        variant='h5'
-                    >
-                        {t('Пароль')}
-                    </Typography>
-                    <Input
-                        placeholder={t<string>('Введите пароль')}
-                        type='password'
-                        value={password}
-                        onChange={onChangePassword}
-                        fullWidth
-                    />
-                </Column>
-                <Column fullWidth align='start' gap='2'>
-                    <Typography
-                        color='disabled'
-                        variant='h5'
-                    >
-                        {t('Повторите пароль')}
-                    </Typography>
-                    <Input
-                        placeholder={t<string>('Введите пароль')}
-                        type='password'
-                        value={password}
-                        onChange={onChangePassword}
-                        fullWidth
-                    />
-                </Column>
-                <Button
-                    className={cls.btn}
-                    type='button'
-                    onClick={onRegistrationClick}
-                    fullWidth
-                >
-                    {t('Зарегистрироваться')}
-                </Button>
-                <Typography
-                    align='center'
-                    className={cls.privacy}
-                    color='disabled'
-                    variant='h6'
-                >
-                    {t('Нажимая кнопку "Зарегистрироваться" вы принимаете')}
-                    <AppLink to='/'>
-                        {' '}
-                        {t('пользовательское соглашение')}
-                    </AppLink>
-                </Typography>
-            </Column>
+            <AuthCard
+                className={className}
+                AuthIcon={AuhtPersonIcon}
+                title={t('Зарегистрируйтесь и получите доступ к файлам')}
+                authButtonTitle={t('Зарегистрироваться')}
+                onAuthClick={onRegistrationClick}
+                inputs={inputs}
+            />
         </DynamicModuleLoader>
     );
 });
